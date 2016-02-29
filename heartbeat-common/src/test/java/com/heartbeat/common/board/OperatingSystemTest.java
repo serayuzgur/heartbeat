a3443fafe6a55a64da2d4de67560846969364a80
Seray Uzgur <seray.uzgur@mebitech.com>
2016-02-29 13:59:56 +0200

    Better coverage

--- heartbeat-common/src/test/java/com/heartbeat/common/board/OperatingSystemTest.java
+++ heartbeat-common/src/test/java/com/heartbeat/common/board/OperatingSystemTest.java
@@ -1,32 +1,42 @@
 package com.heartbeat.common.board;
 
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 
+@FixMethodOrder
 public class OperatingSystemTest {
+    @Test
+    public void testGetType1() throws Exception {
+
+        assert OperatingSystem.getType() != null;
+        //get from static
+        assert OperatingSystem.getType() != null;
+        assert OperatingSystem.getType().getCmdPrefix() != null;
+    }
 
     @Test
     public void testGetTypeLinux() throws Exception {
         System.setProperty("os.name", "Linux");
-        OperatingSystem.Type.LINUX.equals(OperatingSystem.getType());
+        assert OperatingSystem.Type.LINUX.equals(OperatingSystem.getType(true));
     }
 
     @Test
     public void testGetTypeMac() throws Exception {
 
         System.setProperty("os.name", "mac");
-        OperatingSystem.Type.MAC_OS.equals(OperatingSystem.getType());
+        assert OperatingSystem.Type.MAC_OS.equals(OperatingSystem.getType(true));
 
     }
 
     @Test
     public void testGetTypeWindows() throws Exception {
         System.setProperty("os.name", "Windows");
-        OperatingSystem.Type.WINDOWS.equals(OperatingSystem.getType());
+        assert OperatingSystem.Type.WINDOWS.equals(OperatingSystem.getType(true));
     }
 
     @Test
     public void testGetTypeOther() throws Exception {
         System.setProperty("os.name", "generic");
-        OperatingSystem.Type.OTHER.equals(OperatingSystem.getType());
+        assert OperatingSystem.Type.OTHER.equals(OperatingSystem.getType(true));
     }
 }
